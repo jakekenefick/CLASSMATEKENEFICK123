cbe9a86cfa3d6ae61dae2ff3a6d1a4ea9cba7f54
Jake Kenefick <jake.kenefick24@d155.org>
2023-10-11 14:28:00 -0700

    Initial Commit

---
+++ CLASSMATEKENEFICK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLASSMATEKENEFICK
+//
+//  Created by JAKE KENEFICK on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
